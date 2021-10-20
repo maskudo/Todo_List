0e66f7e4231e5f68b33bfdb6dbb5dbb0693ef80e
maskudo <manujkarki101@gmail.com>
2021-10-20 19:39:12 +0545

    initialize main-body

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./styles.css"
+import {initSidebar} from "./modules/sidebar.js"
+import {initMainContent} from "./modules/mainContent.js"
 
+function initMainBody(){
+    initSidebar()
+    initMainContent()
+}
+
+initMainBody()
