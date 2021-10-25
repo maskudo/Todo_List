671c2d81307527ccaa0fc142d1d2cad11207c1a2
maskudo <manujkarki101@gmail.com>
2021-10-25 21:52:05 +0545

    delete createHome, refactor createForm

--- src/modules/sidebar.js
+++ src/modules/sidebar.js
@@ -1,3 +1,6 @@
+import Project from "./project"
+import TodoList from "./toDo"
+
 const sidebar = document.querySelector(".sidebar")
 const projectBox = document.createElement("div")
 projectBox.id = "project-box"
@@ -5,7 +8,7 @@ projectBox.id = "project-box"
 export function initSidebar(){
     createProjectForm()
     createHeader()
-    createHome()
+    // createHome()
     sidebar.append(projectBox)
 }
 function createHeader(){
@@ -17,43 +20,34 @@ function createHeader(){
     header.append(h1)
     sidebar.append(header)
 }
+// function createHome(){
+//     const home = document.createElement("button")
+//     home.id = "home"
+//     home.classList.add("project")
+//     home.textContent = "Home"
+//     buttonAddEventListener(home)
+//     setActive(home)
+//     projectBox.append(home)
+// }
 
 function createProjectForm(){
     const form = document.createElement("form")
     form.id = "project-form"
+    form.innerHTML = `
+    <input type="text" name = "projectName" placeholder="Enter new Project" id = "project-name-input-field" required="true" minlength="3" maxlength="70">
+    <input type="submit" name="submitButton" value="Create" id="project-submit-button">
+    `
+    sidebar.appendChild(form)
 
-    const textField = document.createElement("input")
-    textField.name = "projectName"
-    textField.type = "text"
-    textField.id = "project-name-input-field"
-    textField.placeholder = "Enter New Project"
-    textField.required = true
-
-    const submitButton = document.createElement("input")
-    submitButton.name = "submitButton"
-    submitButton.type = "submit"
-    submitButton.value = "Create"
-    submitButton.id = "project-submit-button"
+    const submitButton = document.querySelector("#project-submit-button")
     submitButton.addEventListener("click",e=>{
         e.preventDefault()
-        createProjectButton()
+        //add project
+        //display project
     })
-
-    form.appendChild(textField)
-    form.appendChild(submitButton)
-
-    sidebar.appendChild(form)
+    
 }
 
-function createHome(){
-    const home = document.createElement("button")
-    home.id = "home"
-    home.classList.add("project")
-    home.textContent = "Home"
-    buttonAddEventListener(home)
-    setActive(home)
-    projectBox.append(home)
-}
 
 function setActive(activeButton){
     const projectButtons = document.querySelectorAll(".project")
@@ -70,30 +64,33 @@ function buttonAddEventListener(button){
         setActive(button)
     })
 }
-function createProjectButton(){
-    const inputField = document.querySelector("#project-name-input-field")
-        const projectName = inputField.value 
-        if(projectName.length === 0 || projectBox.length>25){
-            alert("Project name length should be between 1 and 25")
-            return
-        }
-        const button = document.createElement("button")
-        const leftSide = document.createElement("div")
-        const rightSide = document.createElement("button")
+function displayProjects(){
 
-        leftSide.textContent = `${projectName}`
-        leftSide.className = "project-button-left-side"
-        rightSide.textContent = "X"
-        rightSide.className = "project-button-right-side"
-        rightSide.addEventListener("click", () => {
-            button.remove()
-        })
-
-        button.classList.add("project")
-        buttonAddEventListener(button)
-        inputField.value = ""
-
-        button.append(leftSide)
-        button.append(rightSide)
-        projectBox.append(button)
 }
+// function createProjectButton(){
+//     const inputField = document.querySelector("#project-name-input-field")
+//         const projectName = inputField.value 
+//         if(projectName.length === 0 || projectBox.length>25){
+//             alert("Project name length should be between 1 and 25")
+//             return
+//         }
+//         const button = document.createElement("button")
+//         const leftSide = document.createElement("div")
+//         const rightSide = document.createElement("button")
+
+//         leftSide.textContent = `${projectName}`
+//         leftSide.className = "project-button-left-side"
+//         rightSide.textContent = "X"
+//         rightSide.className = "project-button-right-side"
+//         rightSide.addEventListener("click", () => {
+//             button.remove()
+//         })
+
+//         button.classList.add("project")
+//         buttonAddEventListener(button)
+//         inputField.value = ""
+
+//         button.append(leftSide)
+//         button.append(rightSide)
+//         projectBox.append(button)
+// }
